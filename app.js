c9454180826bd2f0afa825033f127f34b555098e
AaronCGoidel <agoidel2019@mka.org>
2017-09-02 20:01:58 -0400

    Started routing for articles

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ r = require('rethinkdb');
 var config = require(__dirname + '/config.js');
 
 var connection = null;
+
 r.connect( config.rethinkdb, function(err, conn) {
     if (err) throw err;
     connection = conn;
@@ -25,6 +26,19 @@ router.use(function (req,res,next) {
 router.get("/",function(req,res){
     res.sendFile(path + "index.html");
 
+});
+
+router.get("/article/*",function(req, res){
+    res.sendfile(path + "test.html");
+    var getID = /[^/]*$/.exec(req.path)[0];
+    console.log(getID);
+    r.table('articles').filter(r.row('id').eq(getID)).run(connection, function(err, cursor) {
+        if (err) throw err;
+        cursor.toArray(function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        });
+    });
 });
 
 app.use("/",router);
