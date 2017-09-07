075873f61292a793d5e8ea9d53f6f1e746099580
AaronCGoidel <agoidel2019@mka.org>
2017-09-07 18:08:26 -0400

    trying to figure this out

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
-var pg = require('pg');
 
 var config = require(__dirname + '/config.js');
 
@@ -9,6 +8,8 @@ app.use(express.static(__dirname + '/public'));
 
 
 var PORT = config.express.port;
+
+app.set("view engine", "pug");
 
 router.use(function (req,res,next) {
     console.log("/" + req.method);
@@ -18,18 +19,6 @@ router.use(function (req,res,next) {
 router.get("/", function(req, res){
     console.log("test");
     res.render("404");
-});
-
-router.get('/db', function (request, response) {
-    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-        client.query('SELECT * FROM test_table', function(err, result) {
-            done();
-            if (err)
-            { console.error(err); response.send("Error " + err); }
-            else
-            { response.render('pages/db', {results: result.rows} ); }
-        });
-    });
 });
 
 
