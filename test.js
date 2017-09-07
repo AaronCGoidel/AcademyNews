cbf82d70673670241818b6b25fd80f6ae2cf8d38
AaronCGoidel <agoidel2019@mka.org>
2017-09-07 17:56:30 -0400

    just trying pg

--- test.js
+++ test.js
@@ -16,18 +16,18 @@ router.use(function (req,res,next) {
 });
 
 
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-    if (err) throw err;
-    console.log('Connected to postgres! Getting schemas...');
-
-    client
-        .query('SELECT table_schema,table_name FROM information_schema.tables;')
-        .on('row', function(row) {
-            console.log(JSON.stringify(row));
+router.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
         });
+    });
 });
+
 
 app.use("/",router);
 
