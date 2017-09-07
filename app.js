b1ee6a2ceca1579e36231fbefb7f557e98bdb429
AaronCGoidel <agoidel2019@mka.org>
2017-09-07 17:46:16 -0400

    wtf is pg

--- app.js
+++ app.js
@@ -15,6 +15,20 @@ r.connect( config.rethinkdb, function(err, conn) {
 
 app.use(express.static(__dirname + '/public'));
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+    if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client
+        .query('SELECT table_schema,table_name FROM information_schema.tables;')
+        .on('row', function(row) {
+            console.log(JSON.stringify(row));
+        });
+});
+
 var PORT = config.express.port;
 
 app.set("view engine", "pug");
