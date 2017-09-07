577bb71af8d712f89b603efaf85f8b3f1aa72e78
AaronCGoidel <agoidel2019@mka.org>
2017-09-06 23:46:04 -0400

    deleted vestigial swiper code

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -12,21 +12,3 @@ function openNav() {
 function closeNav() {
     document.getElementsByTagName("body")[0].className = "";
 }
-
-(function initMainSlider() {
-    var main_slider = new Swiper('.main-slider > .swiper-container', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 0,
-        loop: true,
-        autoHeight: true
-    });
-
-    var section_slider = new Swiper('.content-slider > .swiper-container', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 30,
-        loop: true,
-        simulateTouch: false
-    });
-})();
