3f9c4d23470409e8a2d45426d317714b27ec6c92
AaronCGoidel <agoidel2019@mka.org>
2017-09-10 11:49:30 -0400

    begin moving to pg

--- app.js
+++ app.js
@@ -1,59 +1,85 @@
 var express = require("express");
 var app = express();
 var router = express.Router();
+var Sequelize = require("sequelize");
 
-r = require('rethinkdb');
+console.log("USED")
 
 var config = require(__dirname + '/config.js');
 
 var connection = null;
 
-r.connect( config.rethinkdb, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-});
-
 app.use(express.static(__dirname + '/public'));
 
 
-var PORT = process.env.PORT || config.express.port;;
+var PORT = process.env.PORT || config.express.port;
 
 app.set("view engine", "pug");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL || config.data.URL);
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('[DATABASE]Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('[DATABASE]Unable to connect to the database:', err);
+    });
+
+const Article = sequelize.define('article', {
+    Author: {
+        type: Sequelize.STRING
+    },
+    id: {
+        type: Sequelize.STRING,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING
+    },
+    blurb: {
+        type: Sequelize.STRING
+    },
+    content: {
+        type: Sequelize.STRING
+    }
+});
 
 router.use(function (req,res,next) {
     console.log("/" + req.method);
     next();
 });
 
-router.get("/", async (req,res, next) => {
-    try {
-        const cursor = await r.table('articles')
-            .filter(r.row('id').eq(config.featured.article1ID)
-                .or(r.row('id').eq(config.featured.article2ID))
-                .or(r.row('id').eq(config.featured.article3ID)))
-            .run(connection);
-        const featuredPosts = await cursor.toArray();
-        res.render("index", {featuredPosts})
-    }catch(err){
-        next(err);
-    }
-});
+// router.get("/", async (req,res, next) => {
+//     try {
+//         const cursor = await r.table('articles')
+//             .filter(r.row('id').eq(config.featured.article1ID)
+//                 .or(r.row('id').eq(config.featured.article2ID))
+//                 .or(r.row('id').eq(config.featured.article3ID)))
+//             .run(connection);
+//         const featuredPosts = await cursor.toArray();
+//         res.render("index", {featuredPosts})
+//     }catch(err){
+//         next(err);
+//     }
+// });
 
 router.get("/article/test", function(req, res){
     res.render("articleTest");
 });
 
-router.get("/article/*", function(req, res) {
-    var getID = /[^/]*$/.exec(req.path)[0];
-    r.table('articles').filter(r.row('id').eq(getID)).run(connection, function(err, cursor) {
-        if (err) throw err;
-        cursor.toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.render("article", {result})
-        });
-    });
-});
+// router.get("/article/*", function(req, res) {
+//     var getID = /[^/]*$/.exec(req.path)[0];
+//     r.table('articles').filter(r.row('id').eq(getID)).run(connection, function(err, cursor) {
+//         if (err) throw err;
+//         cursor.toArray(function(err, result) {
+//             if (err) throw err;
+//             console.log(result);
+//             res.render("article", {result})
+//         });
+//     });
+// });
 
 router.get("/upload", function(req, res) {
     res.render("upload")
--- config.js
+++ config.js
@@ -8,6 +8,9 @@ module.exports = {
     express: {
         port: 5000
     },
+    data: {
+        URL: "postgres://bapqyhgkliogph:f60b8ee0c60b874622def709e9b259a954a4f8046d8024e3c63bbd9622d10fbe@ec2-184-73-189-190.compute-1.amazonaws.com:5432/d4ntsetj69f624"
+    },
     featured: {
         article1ID: '001',
         article2ID: '002',
