5d8d90a51a3d73d151c66525deed0d3b62e4e5c7
AaronCGoidel <agoidel2019@mka.org>
2017-09-10 12:18:55 -0400

    two test at once

--- app.js
+++ app.js
@@ -51,6 +51,31 @@ router.use(function (req,res,next) {
     next();
 });
 
+
+// force: true will drop the table if it already exists
+Article.sync({force: false}).then(() => {
+    // Table created
+    return Article.create({
+        author: "Arthur Dent",
+        id: "001",
+        title: "Test Article",
+        blurb: "A quick summary",
+        content: "This is a super interesting article full of fantastic journalism."
+    });
+});
+
+// force: true will drop the table if it already exists
+Article.sync({force: false}).then(() => {
+    // Table created
+    return Article.create({
+        author: "Kevin Spacey",
+        id: "002",
+        title: "Democracy is so Overrated",
+        blurb: "You build your future, it isn't handed to you",
+        content: "There are two kinds of pain. The sort of pain that makes you strong, or useless pain. The sort of pain that’s only suffering. I have no patience for useless things."
+    });
+});
+
 Article.findAll().then(data => {
     console.log(data)
 });
